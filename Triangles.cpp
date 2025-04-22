ec2b3852bc31d48c7407f3d9f130bfb45648891d
Dirk Zimmermann <me@dirkz.com>
2025-04-22 12:47:35 +0200

    Can create a device

--- Triangles.cpp
+++ Triangles.cpp
@@ -6,12 +6,29 @@ namespace triangles
 Triangles::Triangles()
 {
     sdl::Init(SDL_INIT_VIDEO);
+
     SDL_DisplayID display = sdl::GetPrimaryDisplay();
     const SDL_DisplayMode *mode = sdl::GetCurrentDisplayMode(display);
     int width = mode->w / 2;
     int height = mode->h / 2;
+
     m_window = sdl::CreateWindow("Triangles", width, height, 0);
     sdl::SetWindowPosition(m_window, mode->w / 4, 100);
+
+#ifdef _DEBUG
+    bool debugMode = true;
+#else
+    bool debugMode = false;
+#endif
+
+    m_device = sdl::CreateGPUDevice(SDL_GPU_SHADERFORMAT_DXIL, true, nullptr);
+    sdl::ClaimWindowForGPUDevice(m_device, m_window);
+}
+
+Triangles::~Triangles()
+{
+    sdl::ReleaseWindowFromGPUDevice(m_device, m_window);
+    sdl::DestroyGPUDevice(m_device);
 }
 
 void Triangles::AppIterate()
--- Triangles.h
+++ Triangles.h
@@ -6,12 +6,14 @@ namespace triangles
 struct Triangles
 {
     Triangles();
+    ~Triangles();
 
     void AppIterate();
     bool AppEvent(SDL_Event *event);
 
   private:
     SDL_Window *m_window;
+    SDL_GPUDevice *m_device;
 };
 
 } // namespace triangles
