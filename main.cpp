495aa61d8b5a0100b268ed3839ee3e9c5cf31d08
Dirk Zimmermann <me@dirkz.com>
2025-04-22 11:00:51 +0200

    Make this function, and only this one, static (intellisense)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 ﻿#define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
+static SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
 {
     return SDL_APP_CONTINUE;
 }
