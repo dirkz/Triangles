0b88e55e8f8873cc6e5318e0b4f7c4c6971f9134
Dirk Zimmermann <me@dirkz.com>
2025-04-23 12:43:31 +0200

    Method order

--- Triangles.h
+++ Triangles.h
@@ -19,12 +19,12 @@ struct Triangles
     std::string PreferredShaderFormatString() const;
     std::string ShaderEntryPoint(SDL_GPUShaderStage stage) const;
 
-    SDL_GPUGraphicsPipelineCreateInfo PipelineCreateInfo(SDL_GPUShader *vertexShader,
-                                                         SDL_GPUShader *fragmentShader) const;
-
     SDL_GPUShader *LoadShader(const std::string &filenameBase, SDL_GPUShaderStage stage,
                               Uint32 numUniformBuffers = 0, Uint32 numSamplers = 0,
                               Uint32 numStorageBuffers = 0, Uint32 numStorageTextures = 0) const;
+
+    SDL_GPUGraphicsPipelineCreateInfo PipelineCreateInfo(SDL_GPUShader *vertexShader,
+                                                         SDL_GPUShader *fragmentShader) const;
 
     SDL_Window *m_window = nullptr;
     SDL_GPUDevice *m_device = nullptr;
