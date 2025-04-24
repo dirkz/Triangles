0f702a499b95b214ea89667280f8cedb68487ae4
Dirk Zimmermann <me@dirkz.com>
2025-04-24 17:30:20 +0200

    Keep the aspect ratio

--- IndexedQuad.cpp
+++ IndexedQuad.cpp
@@ -85,11 +85,16 @@ void IndexedQuad::AppIterate()
             0, 0,  0, 1  // row 4
         };
 
+        float aspect = static_cast<float>(width) / height;
+
+        glm::mat4x4 scale{1};
+        scale = glm::scale(scale, glm::vec3{1.f / aspect, 1.f, 1.f});
+
         // But, GLM adheres to the GLSLang spec, so it expects matrices
         // to be constructed in column-major order (e.g. see here, page 84):
         // https://registry.khronos.org/OpenGL/specs/gl/GLSLangSpec.4.20.pdf
         // So we have to transpose.
-        glm::mat4x4 transformation = glm::transpose(rotation);
+        glm::mat4x4 transformation = glm::transpose(rotation * scale);
 
         sdl::PushGPUVertexUniformData(commandBuffer, 0, &transformation, sizeof(transformation));
 
