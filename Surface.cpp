de6b3250276a0458d1a7bf2917cd66a1afb13146
Dirk Zimmermann <me@dirkz.com>
2025-04-24 19:41:29 +0200

    Maybe able to set pixels on a surface

--- Surface.cpp
+++ Surface.cpp
@@ -3,4 +3,30 @@
 namespace triangles
 {
 
+constexpr int BytesPerPixel = 4;
+
+Surface::Surface(int width, int height)
+{
+    m_surface = sdl::CreateSurface(width, height, SDL_PIXELFORMAT_RGBA8888);
 }
+
+Surface::~Surface()
+{
+    SDL_DestroySurface(m_surface);
+}
+
+void Surface::SetPixel(int x, int y, Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+{
+    Uint32 pixel = r + (g << 8) + (b << 16) + (a << 24);
+    SetPixel(x, y, pixel);
+}
+
+void Surface::SetPixel(int x, int y, Uint32 pixel)
+{
+    Uint8 *base = static_cast<Uint8 *>(m_surface->pixels);
+    Uint8 *row = base + m_surface->pitch * y;
+    Uint32 *pPixel = reinterpret_cast<Uint32 *>(row + x * BytesPerPixel);
+    *pPixel = pixel;
+}
+
+} // namespace triangles
--- Surface.h
+++ Surface.h
@@ -7,6 +7,14 @@ namespace triangles
 
 struct Surface
 {
+    Surface(int width, int height);
+    ~Surface();
+
+    void SetPixel(int x, int y, Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255);
+    void SetPixel(int x, int y, Uint32 pixel);
+
+  private:
+    SDL_Surface *m_surface;
 };
 
 } // namespace triangles
