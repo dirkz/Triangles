197e086b8670c832ee9caf18cae6d682d930b741
Dirk Zimmermann <me@dirkz.com>
2025-04-25 11:48:17 +0200

    These could be 8 gradients

--- Noise.cpp
+++ Noise.cpp
@@ -3,6 +3,12 @@
 namespace triangles
 {
 
+using namespace glm;
+
+constexpr float D = 1.f / 1.41421f;
+constexpr std::array<vec2, 8> Gradients{vec2{0, 1},  vec2{D, D},   vec2{1, 0},  vec2{D, -D},
+                                        vec2{0, -1}, vec2{-D, -D}, vec2{-1, 0}, vec2{-D, D}};
+
 Noise::Noise()
 {
 }
