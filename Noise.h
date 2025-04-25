b4fc110b87c3689d1b26f69b107901641d83dab2
Dirk Zimmermann <me@dirkz.com>
2025-04-25 19:32:28 +0200

    Remove now redundant things

--- Noise.h
+++ Noise.h
@@ -5,15 +5,9 @@
 namespace triangles
 {
 
-constexpr int GradientsRowPitch = 256;
-
 struct Noise
 {
-    Noise();
-
     double operator()(double x, double y, double z);
-
-  private:
 };
 
 } // namespace triangles
