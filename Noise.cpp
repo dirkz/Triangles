f5d8e118622c7581fce290e367528e3d9a5a3da5
Dirk Zimmermann <me@dirkz.com>
2025-04-25 18:52:08 +0200

    That can't be right

--- Noise.cpp
+++ Noise.cpp
@@ -57,6 +57,21 @@ float Noise::operator()(float x, float y)
     int j = static_cast<int>(std::floor(y));
 
     vec2 g00 = Gradient(i, j);
+    vec2 g10 = Gradient(i + 1, j);
+    vec2 g01 = Gradient(i, j + 1);
+    vec2 g11 = Gradient(i + 1, j + 1);
+
+    float u = x - static_cast<float>(i);
+    float v = y - static_cast<float>(j);
+
+    vec2 n00 = g00 * vec2{u, v};
+    vec2 n10 = g10 * vec2{u - 1, v};
+    vec2 n01 = g01 * vec2{u, v - 1};
+    vec2 n11 = g11 * vec2{u - 1, v - 1};
+
+    vec2 nx0 = n00 * (1 - smootherstep(u)) + n10 * smootherstep(u);
+    vec2 nx1 = n01 * (1 - smootherstep(u)) + n11 * smootherstep(u);
+    vec2 nxy = nx0 * (1 - smootherstep(v)) + nx1 * smootherstep(v);
 
     return 0.0f;
 }
