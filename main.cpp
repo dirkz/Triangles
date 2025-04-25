65c796f3b2974100c50f213b514013feb29ea326
Dirk Zimmermann <me@dirkz.com>
2025-04-25 21:09:45 +0200

    From now on, only one main for all

---
+++ main.cpp
@@ -0,0 +1,66 @@
+﻿#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+
+#include "BasicTriangle.h"
+#include "BasicUniform.h"
+#include "IndexedQuad.h"
+#include "TexturedQuad.h"
+
+using namespace triangles;
+
+using App = TexturedQuad;
+
+static SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
+{
+    try
+    {
+        App *app = new App{};
+        *appstate = app;
+    }
+    catch (...)
+    {
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    try
+    {
+        App *app = static_cast<App *>(appstate);
+        app->AppIterate();
+    }
+    catch (...)
+    {
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    try
+    {
+        App *app = static_cast<App *>(appstate);
+        bool shouldQuit = app->AppEvent(event);
+        if (shouldQuit)
+        {
+            return SDL_APP_SUCCESS;
+        }
+    }
+    catch (...)
+    {
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    App *app = static_cast<App *>(appstate);
+    delete app;
+}
