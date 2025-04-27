3bcaed88c093822a48eed20af0174ea3db348b8b
Dirk Zimmermann <me@dirkz.com>
2025-04-27 09:09:08 +0200

    Implement std::less

--- PositionColorTextureVertex.cpp
+++ PositionColorTextureVertex.cpp
@@ -11,7 +11,11 @@ PositionColorTextureVertex::PositionColorTextureVertex(float x, float y, float z
 
 bool PositionColorTextureVertex::operator<(const PositionColorTextureVertex &other) const
 {
-    return false;
+    return m_position.x < other.m_position.x && m_position.y < other.m_position.y &&
+           m_position.z < other.m_position.z && m_color.x < other.m_color.x &&
+           m_color.y < other.m_color.y && m_color.z < other.m_color.z &&
+           m_color.w < other.m_color.w && m_texture.x < other.m_texture.x &&
+           m_texture.y < other.m_texture.y;
 }
 
 } // namespace triangles
