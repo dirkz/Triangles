67bca79a0f5fbbb2775e4013a2e1501e01246adb
Dirk Zimmermann <me@dirkz.com>
2025-04-27 09:14:00 +0200

    Rely on glm comparison

--- PositionColorTextureVertex.cpp
+++ PositionColorTextureVertex.cpp
@@ -11,11 +11,9 @@ PositionColorTextureVertex::PositionColorTextureVertex(float x, float y, float z
 
 bool PositionColorTextureVertex::operator<(const PositionColorTextureVertex &other) const
 {
-    return m_position.x < other.m_position.x && m_position.y < other.m_position.y &&
-           m_position.z < other.m_position.z && m_color.x < other.m_color.x &&
-           m_color.y < other.m_color.y && m_color.z < other.m_color.z &&
-           m_color.w < other.m_color.w && m_texture.x < other.m_texture.x &&
-           m_texture.y < other.m_texture.y;
+    return glm::all(glm::lessThan(m_position, other.m_position)) &&
+           glm::all(glm::lessThan(m_color, other.m_color)) &&
+           glm::all(glm::lessThan(m_texture, other.m_texture));
 }
 
 } // namespace triangles
