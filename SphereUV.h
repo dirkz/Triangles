c9dd79c0d289f12c1f533312664c357bd79b0507
Dirk Zimmermann <me@dirkz.com>
2025-04-29 10:44:12 +0200

    Start SphereUV

--- SphereUV.h
+++ SphereUV.h
@@ -2,11 +2,25 @@
 
 #include "stdafx.h"
 
+#include "IndexedVertexBuffer.h"
+#include "PositionColorTextureVertex.h"
+
 namespace triangles
 {
 
 struct SphereUV
 {
+    using VertexBuffer = IndexedVertexBuffer<PositionColorTextureVertex>;
+
+    SphereUV();
+
+    const VertexBuffer &GetVertexBuffer() const
+    {
+        return m_vertexBuffer;
+    }
+
+  private:
+    VertexBuffer m_vertexBuffer;
 };
 
 } // namespace triangles
