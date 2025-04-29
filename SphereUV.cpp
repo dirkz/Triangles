396130b99aca12988337ac41d0541927a764e8f1
Dirk Zimmermann <me@dirkz.com>
2025-04-29 13:38:16 +0200

    This is one weird sphere

--- SphereUV.cpp
+++ SphereUV.cpp
@@ -31,8 +31,18 @@ SphereUV::SphereUV(FXMVECTOR color)
             XMVECTOR v1 = PointOnSphere(angleXY, angleXZ + stepXZ);
             XMVECTOR v2 = PointOnSphere(angleXY + stepXY, angleXZ + stepXZ);
             XMVECTOR v3 = PointOnSphere(angleXY + stepXY, angleXZ);
-            float u = angleXY + XM_PIDIV2 / XM_PI;
-            float v = angleXZ / XM_2PI;
+
+            float tu0 = (angleXY + XM_PIDIV2) / XM_PI;
+            float tu1 = (angleXY + stepXY + XM_PIDIV2) / XM_PI;
+            float tv0 = angleXZ / XM_2PI;
+            float tv1 = (angleXZ + stepXZ) / XM_2PI;
+
+            PositionColorTextureVertex p0{v0, color, tu0, tv0};
+            PositionColorTextureVertex p1{v1, color, tu1, tv0};
+            PositionColorTextureVertex p2{v2, color, tu0, tv1};
+            PositionColorTextureVertex p3{v3, color, tu1, tv1};
+
+            m_vertexBuffer.Quad(p0, p1, p2, p3);
         }
     }
 }
