9c98bd58909441b4cffdac75ef3c805d72e92c88
Dirk Zimmermann <me@dirkz.com>
2025-04-30 10:56:31 +0200

    G (golden ratio)

--- Icosahedron.cpp
+++ Icosahedron.cpp
@@ -234,6 +234,8 @@ constexpr XMVECTORF32 PlaneXColor = {{{0.61f, 0.53f, 0.88f, 1.f}}};
 constexpr XMVECTORF32 PlaneYColor = {{{0.77f, 0.93f, 0.65f, 1.f}}};
 constexpr XMVECTORF32 PlaneZColor = {{{0.18f, 0.41f, 0.33f, 1.f}}};
 
+const float G = 1.f + std::sqrt(5.f) / 2.f;
+
 void Icosahedron::CreateGeometry()
 {
 }
