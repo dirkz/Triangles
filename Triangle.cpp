ee7b88bab2c4d243f90b3ccac2af56efa563a335
Dirk Zimmermann <me@dirkz.com>
2025-04-30 18:25:50 +0200

    Make triangulation work

--- Triangle.cpp
+++ Triangle.cpp
@@ -39,9 +39,9 @@ std::array<Triangle, 4> Triangle::Triangulate() const
     XMVECTOR e2 = XMVectorSubtract(m_v0, m_v2);
     XMVECTOR e3 = XMVectorSubtract(m_v2, m_v1);
 
-    XMVECTOR m1 = XMVectorScale(e1, 0.5f);
-    XMVECTOR m2 = XMVectorScale(e2, 0.5f);
-    XMVECTOR m3 = XMVectorScale(e3, 0.5f);
+    XMVECTOR m1 = XMVectorAdd(m_v0, XMVectorScale(e1, 0.5f));
+    XMVECTOR m2 = XMVectorAdd(m_v2, XMVectorScale(e2, 0.5f));
+    XMVECTOR m3 = XMVectorAdd(m_v1, XMVectorScale(e3, 0.5f));
 
     return {Triangle{m_v0, m1, m2}, Triangle{m1, m3, m2}, Triangle{m1, m_v1, m3},
             Triangle{m3, m_v2, m2}};
